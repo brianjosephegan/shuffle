8fbd86f5982bf9a57478acabb0c38daeb7fde130
Brian Egan <brianjoseph.egan@gmail.com>
2018-08-21 17:44:33 -0700

    First draft of Shuffle
    Includes working application and icon.

---
+++ Shuffle/MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Views;
+
+namespace Shuffle
+{
+    [Activity(Label = "Shuffle", MainLauncher = true,  Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        /// <summary>
+        /// Called when the activity is starting
+        /// </summary>
+        /// <param name="savedInstanceState">The instance state</param>
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            try
+            {
+                AssetManager assetManager = this.Assets;
+                Shuffler game = new Shuffler(assetManager.List("Cards"));
+
+                ImageButton cardViewer = FindViewById<ImageButton>(Resource.Id.CardViewer);
+                cardViewer.Click += (o, e) =>
+                {
+                    string nextCard = game.NextCard();
+                    if (nextCard != null)
+                    {
+                        UpdateImage(cardViewer, nextCard);
+                    }
+                    else
+                    {
+                        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                        alertDialog.SetTitle("Shuffle");
+                        alertDialog.SetMessage("No more cards. Restart?");
+                        alertDialog.SetPositiveButton("OK", (senderAlert, args) => { UpdateImage(cardViewer, game.Start()); });
+                        alertDialog.SetNegativeButton("Cancel", (senderAlert, args) => { });
+                        alertDialog.Show();
+                    }
+                };
+                UpdateImage(cardViewer, game.Start());
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Failed to perform setup for Kings. Exception = {0}", ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Updates the specified ImageButton with the specified image
+        /// </summary>
+        /// <param name="imageButton">ImageButton to update image for</param>
+        /// <param name="image">Path to image under AssetManager</param>
+        private void UpdateImage(ImageButton imageButton, string image)
+        {
+            AssetManager assetManager = this.Assets;
+            Drawable currentImage = imageButton.Drawable;
+            Drawable nextImage;
+            using (Stream imageStream = assetManager.Open(Path.Combine("Cards", image)))
+            {
+                nextImage = Drawable.CreateFromStream(imageStream, null);
+            }
+            imageButton.SetImageDrawable(nextImage);
+            currentImage?.Dispose();
+        }
+    }
+}
+
---
+++ Shuffle/Shuffler.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Shuffle
+{
+    /// <summary>
+    /// The logic for shuffling a deck of cards
+    /// </summary>
+    internal class Shuffler
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Kings.Game"/> class.
+        /// </summary>
+        /// <param name="deck">Deck to use for the game</param>
+        public Shuffler(IEnumerable<string> deck)
+        {
+            if (deck == null)
+            {
+                throw new ArgumentNullException(nameof(deck), "Deck cannot be null.");
+            }
+
+            if (!deck.Any())
+            {
+                throw new ArgumentException("Deck cannot be empty.", nameof(deck));
+            }
+
+            if (deck.Any(x => x == null))
+            {
+                throw new ArgumentException("Deck cannot contain any null values.", nameof(deck));
+            }
+
+            this.deck = deck;
+        }
+
+        /// <summary>
+        /// Starts the game
+        /// </summary>
+        /// <returns>The first card in the deck</returns>
+        public string Start()
+        {
+            currentDeck = ShuffleDeck(deck);
+            currentDeck.MoveNext();
+            return currentDeck.Current;
+        }
+
+        /// <summary>
+        /// Returns the next card in the deck
+        /// </summary>
+        /// <returns>The next card or null if no cards are left</returns>
+        public string NextCard()
+        {
+            if (currentDeck.MoveNext())
+            {
+                return currentDeck.Current;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Shuffles the specified deck
+        /// </summary>
+        /// <param name="deckToShuffle">The deck to shuffle</param>
+        /// <returns>A shuffled deck</returns>
+        private IEnumerator<string> ShuffleDeck(IEnumerable<string> deckToShuffle)
+        {
+            return deckToShuffle.OrderBy(x => random.Next()).ToList().GetEnumerator();
+        }
+
+        /// <summary>
+        /// Current deck being used in the game
+        /// </summary>
+        private IEnumerator<string> currentDeck;
+
+        /// <summary>
+        /// The deck of cards to use for a game
+        /// </summary>
+        private readonly IEnumerable<string> deck;
+
+        /// <summary>
+        /// Random object used for shuffling the cards
+        /// </summary>
+        private readonly Random random = new Random();
+    }
+}
